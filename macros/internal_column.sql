a1863e624a725f591edad8881902bcfffc380eb7
Matt Zahar <matthew.zahar@ncino.com>
2024-06-05 09:25:13 -0400

    create macros and model

---
+++ macros/internal_column.sql
@@ -0,0 +1,10 @@
+{% macro internal_columns() %}
+    ,internal_uai0060_data_datetime
+    ,internal_uai0060_year
+    ,internal_uai0060_day
+    ,internal_uai0060_correlation_id
+    ,internal_uai0060_tenant_context
+    ,internal_uai0060_account
+    ,internal_uai0060_month
+    ,internal_uai0060_region
+{% endmacro %}
--- models/fedfis/bank_financials_quarterly_reports_original_names.sql
+++ models/fedfis/bank_financials_quarterly_reports_original_names.sql
@@ -1,3 +1,4 @@
 select
-     {{ column_mapping('bank_financials_quarterly_reports') }}
+     {{ column_mapping('bank_financials_quarterly_reports', 'b') }}
+     {{ internal_columns() }}
 from {{ source("fedfis", "bank_financials_quarterly_reports") }} b
